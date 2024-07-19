aeaf3aab69da6ef50d09922b7a8d9b9890ffba5f
Yuma Shiraishi <135203168+yuma-shiraishi-yoriso@users.noreply.github.com>
2024-07-19 09:54:03 +0000

    Change: /user は ok だけ返すように修正

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -4,5 +4,5 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+    return 'ok';
+});
