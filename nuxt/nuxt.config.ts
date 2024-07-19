0b3b4bca5ffa7a58c64c65b1d60edae24fe6600a
Yuma Shiraishi <135203168+yuma-shiraishi-yoriso@users.noreply.github.com>
2024-07-19 09:53:34 +0000

    Config: codeworkspace 上で API と疎通できるように設定

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      githubToken: process.env.GITHUB_TOKEN
+    }
+  },
+  nitro: {
+    routeRules: {
+      '/api/**': {
+        proxy: `https://${process.env.CODESPACE_NAME}.app.github.dev/api/**`,
+      },
+    },
+  },
   devtools: { enabled: true },
   compatibilityDate: "2024-07-10"
 })
