bace1697c9d98238695c67305050f7863e9de9c5
Yuma Shiraishi <135203168+yuma-shiraishi-yoriso@users.noreply.github.com>
2024-07-23 08:46:45 +0000

    Tasks API 追加

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return 'ok';
 });
+
+Route::resource('tasks', TaskController::class);
